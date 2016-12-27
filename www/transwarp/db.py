d9ba8310703dabc1cbed1ba92a4536698f9027f5
liuxiaobai <zwsibian@163.com>
2016-12-27 17:42:47 +0800

    Create db.py

---
+++ www/transwarp/db.py
@@ -0,0 +1,28 @@
+#! /bin/python
+# -*-coding:utf-8-*-
+
+def select():
+    pass
+
+def update():
+    pass
+
+def insert():
+    pass
+
+def delete():
+    pass
+
+engine = None
+
+def create_engine():
+    pass
+
+class _Engine(object):
+    pass
+
+def connection():
+    pass
+
+def transaction():
+    pass
